02889266a800276e131b537328bdbcec6fdd70e6
thbl088 <thb8@outlook.fr>
2021-03-28 16:16:57 +0200

    ajout de choses, peut etre

--- src/arith.ml
+++ src/arith.ml
@@ -3,3 +3,24 @@
 open Expression_scanner;;
 
 string_to_token_list "34 56 2+ x * -;";;
+string_to_token_list "x 3 + 5 7 + + 3 4 * 1 3 + / /;";;
+type operator =
+  |Plus
+  |Minus
+  |Mult
+  |Div
+;;
+
+type tree =
+  |Var of char
+  |Cst of int
+  |Unary of tree
+  |Binary of operator * tree * tree
+;;
+
+let rec parse (tokens : token list) : tree =
+  match tl with
+  |End -> continuer
+  |
+;;
+
